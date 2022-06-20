04741cf85bdcc7f366bdac847cb0ae2891cd540a
BTech <daffydanni@gmail.com>
2022-06-20 11:16:07 +0100

    style and function

--- function/index.js
+++ function/index.js
@@ -1,17 +1,17 @@
 let increment = 30;
-for(let i = 0; i < 11; i++) {
-    let w = i * increment;
-    let hours = document.querySelectorAll('.hours');
-    let s = (hours[i].style.transform = "rotate(" + w + "deg)");
+for (let i = 0; i <= 11; i++) {
+  let w = i * increment;
+  let hours = document.querySelectorAll(".hours");
+  let s = (hours[i].style.transform = "rotate(" + w + "deg)");
 }
 let timing = setInterval(time, 1000);
-functiontime(){
-    let arms = document.querySelectorAll(".arms");
-    let date = new Date();
-    let hour = date.getHours() * 30;
-    let min = date.getMinutes() * 6;
-    let sec = date.getSeconds() * 6;
-    arms[0].style.transform = "rotateZ(" + hour + "deg)";
-    arms[1].style.transform = "rotateZ(" + min + "deg)";
-    arms[2].style.transform = "rotateZ(" + sec + "deg)";
+function time() {
+  let arms = document.querySelectorAll(".arms");
+  let date = new Date();
+  let hour = date.getHours() * 30;
+  let min = date.getMinutes() * 6;
+  let sec = date.getSeconds() * 6;
+  arms[0].style.transform = "rotateZ(" + hour + "deg)";
+  arms[1].style.transform = "rotateZ(" + min + "deg)";
+  arms[2].style.transform = "rotateZ(" + sec + "deg)";
 }
